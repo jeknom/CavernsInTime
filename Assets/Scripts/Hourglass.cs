beb2e55f6fa95c03e693df390dfbeb39e645e902
Johannes Palvanen <palvanenj@gmail.com>
2019-11-19 23:20:02 +0200

    Added hourglass sprite and rotation tween

---
+++ Assets/Scripts/Hourglass.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class Hourglass : MonoBehaviour
+{
+    [SerializeField] float rotateDuration = 1f;
+    bool isRotating = false;
+
+    void Turn()
+    {
+        if (!this.isRotating)
+        {
+            this.isRotating = true;
+            var endValue = new Vector3(0f, 0f, 180f);
+            var tween = DOTween.Play(this.transform);
+            this.transform
+                .DORotate(
+                    endValue,
+                    rotateDuration,
+                    RotateMode.FastBeyond360)
+                .OnStart(() => this.isRotating = true)
+                .SetRelative()
+                .OnComplete(() => this.isRotating = false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            this.Turn();
+        }
+    }
+}
