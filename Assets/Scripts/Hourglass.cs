5e266dc266078ffa12482626d9b06a4f38c5fb41
Johannes Palvanen <palvanenj@gmail.com>
2019-11-20 00:26:39 +0200

    Add hourglass animation

--- Assets/Scripts/Hourglass.cs
+++ Assets/Scripts/Hourglass.cs
@@ -3,32 +3,47 @@ using DG.Tweening;
 
 public class Hourglass : MonoBehaviour
 {
-    [SerializeField] float rotateDuration = 1f;
+    [SerializeField] LevelData data;
+    [SerializeField] float rotateDuration;
     bool isRotating = false;
 
-    void Turn()
+    public void Rotate()
     {
         if (!this.isRotating)
         {
             this.isRotating = true;
             var endValue = new Vector3(0f, 0f, 180f);
-            var tween = DOTween.Play(this.transform);
             this.transform
                 .DORotate(
                     endValue,
-                    rotateDuration,
+                    this.rotateDuration,
                     RotateMode.FastBeyond360)
-                .OnStart(() => this.isRotating = true)
+                .OnStart(() => this.OnRotateStart())
                 .SetRelative()
-                .OnComplete(() => this.isRotating = false);
+                .OnComplete(() => this.OnRotateComplete());
         }
+    }
+
+    void OnRotateStart()
+    {
+        this.isRotating = true;
+    }
+
+    void OnRotateComplete()
+    {
+        this.isRotating = false;
+    }
+
+    public void StartTimer()
+    {
+
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            this.Turn();
+            this.Rotate();
         }
     }
 }
