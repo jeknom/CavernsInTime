ac0f43756127523a0da17312fb6fcdd129d44833
Johannes Palvanen <palvanenj@gmail.com>
2020-01-25 16:50:56 +0200

    Added options

---
+++ Assets/Scripts/Option.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Utils
+{
+    public class Option<T>
+    {
+        readonly bool hasValue;
+        readonly T value;
+
+        public Option(T value = default) {
+            this.hasValue = value != null;
+            this.value = value;
+        }
+
+        public void Match(Action<T> some, Action none)
+        {
+            if (this.hasValue)
+            {
+                some(this.value);
+            }
+            else
+            {
+                none();
+            }
+        }
+
+        public void MatchSome(Action<T> f)
+        {
+            if (this.hasValue)
+            {
+                f(this.value);
+            }
+        }
+
+        public void MatchNone(Action f)
+        {
+            if (!this.hasValue)
+            {
+                f();
+            }
+        }
+    }
+}
